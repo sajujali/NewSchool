d93d1dcc733fd4ef5b269803089afc38ba673a6c
sajujali <saju.jali@gmail.com>
2023-04-13 12:52:58 +0530

    Weather Change 2

--- NewSchool.Api.Assessment/Controllers/WeatherForecastController.cs
+++ NewSchool.Api.Assessment/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace NewSchool.Api.Assessment.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Pleasant"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Pleasant1"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
